d557d29bd978f9336a38343c2bcf19ff430700b1
aqxys <aqxyss@gmail.com>
2022-04-11 17:12:20 +0300

    Homework_2

---
+++ homework_1104_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// На входе три числа, на выходе показать наибольшее
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if(number2 > number1)
+{ 
+    max = number2;
+} 
+if(number3 > number2)
+{
+     max = number3;
+}
+Console.Write("Наибольшее число: ");
+Console.WriteLine(max);
