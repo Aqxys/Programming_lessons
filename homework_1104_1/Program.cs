a914e441d2c28c51a56dfcf4d58d2c7efdf3d10e
aqxys <aqxyss@gmail.com>
2022-04-11 16:55:02 +0300

    Homework_1

---
+++ homework_1104_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// На входе два числа, на выходе показать наибольшее и наименьшее
+Console.WriteLine("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number > number2)
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(number);
+    Console.Write("Наименьшее число: ");
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(number2);
+    Console.Write("Наименьшее число: ");
+    Console.WriteLine(number);
+}
