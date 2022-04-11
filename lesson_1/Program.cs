a58e4bc975d223b52a40d978c9990fabf2d63e0e
aqxys <aqxyss@gmail.com>
2022-04-11 15:11:12 +0300

    Добавлено задание о квадрате чисел

---
+++ lesson_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Проверка чисел на квадрат
+Console.WriteLine("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+if  (result ==number2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
