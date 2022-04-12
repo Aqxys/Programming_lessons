19a9146800d97f81eebf329b80c3fdd45cc8de42
aqxys <aqxyss@gmail.com>
2022-04-12 11:28:09 +0300

    Homework_4

---
+++ homework_1104_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// На входе число, на выходе все четные от 1 до N
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while(count < number) 
+{
+    count +=2;
+    Console.Write(count);
+    Console.Write(", ");
+}
+
