ed1f58bcd4718eaa657acf8c754bf0506f6efd64
aqxys <aqxyss@gmail.com>
2022-04-12 10:49:41 +0300

    Homework_3

---
+++ homework_1104_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверка числа на четность, на выводе результат проверки
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int checknumber = number % 2;
+if(checknumber == 0) 
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
