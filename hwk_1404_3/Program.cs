09d1eb9c7c7a10e2e9715ab86e2a6735df64f274
aqxys <aqxyss@gmail.com>
2022-04-14 17:18:10 +0300

    123

---
+++ hwk_1404_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number3 = (number / 100) % 10;
+if (number < 100)
+    Console.WriteLine(number + " -> третьей цифры нет");
+else
+    Console.WriteLine(number + " -> " + number3);
