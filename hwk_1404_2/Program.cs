fa37ddc38d139e79f5091dc850b0f4b82bc456cd
aqxys <aqxyss@gmail.com>
2022-04-14 15:58:39 +0300

    Homework_1404_2

---
+++ hwk_1404_2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа
+
+int number = new Random().Next(1, 1000);
+int result = ((number / 100)*10) + (number % 10);
+Console.WriteLine(number + " -> " + result);
