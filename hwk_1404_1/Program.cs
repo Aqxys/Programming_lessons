b72b420902c155df8e2eb3bce538275e0b5d02f2
aqxys <aqxyss@gmail.com>
2022-04-14 15:38:10 +0300

    add gitignore

--- hwk_1404_1/Program.cs
+++ hwk_1404_1/Program.cs
@@ -2,5 +2,4 @@
 // и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int digit2 = (number / 10) % 10;
-Console.WriteLine("Для числа " + number + " второе число: " + digit2);
+Console.WriteLine("Для числа " + number + " второе число: " + ((number / 10) % 10));
