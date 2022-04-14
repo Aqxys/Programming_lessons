017d61cf43d05f95a1960ba1376f7103fe1f971b
aqxys <aqxyss@gmail.com>
2022-04-14 15:22:00 +0300

    add homework 1404

---
+++ hwk_1404_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit2 = (number / 10) % 10;
+Console.WriteLine("Для числа " + number + " второе число: " + digit2);
