521b5d7a87f8c4b2d92c85e8df7034cdd5041c00
aqxys <aqxyss@gmail.com>
2022-04-14 14:31:26 +0300

    add finder for 7 and 23 compatible numbers

---
+++ seminar_2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Поиск в массиве чисел кратных 7 и 23
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    int count = 0;
+    while (index < length)
+    {
+        count += 1;
+        collection[index] = count;
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+int[] array = new int[10000];
+int counter = 0;
+int lenarr = array.Length;
+int index = 0;
+
+FillArray(array);
+while (counter < lenarr)
+{
+    if ((array[index] % 7 == 0) && (array[index] % 23 == 0))
+    {
+        Console.Write(array[index] + " ");
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+    counter++;
+}
