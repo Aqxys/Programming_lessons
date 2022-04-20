656cabb01e231515765089762f36435ffaa4a3b4
aqxys <aqxyss@gmail.com>
2022-04-20 17:39:30 +0300

    end of homework

---
+++ Task19/Program19.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом
+string number1;
+for (;;)
+{
+    Console.WriteLine("Введите пятизначное число: ");
+    number1 = Console.ReadLine();
+        if (number1.Length > 5 || number1.Length < 5) //проверка, что длина числа именно 5 цифр
+        {
+            Console.WriteLine("Некорректный ввод, попробуйте еще раз");
+        }
+        else break;
+        
+}
+int number = Convert.ToInt32(number1);
+int digit1 = number / 10000;
+int digit2 = (number / 1000) % 10;
+int digit4 = (number / 10) % 10;
+int digit5 = number % 10;
+if ((digit1 == digit5) && (digit2 == digit4))
+    //вариант без лишних переменных 
+    //if (((number / 10000) == (number % 10)) && (((number / 1000) % 10) == ((number / 10) % 10))))
+    //но в нем много скобок, в них легко запутаться
+    Console.WriteLine(number + "-> да");
+else
+    Console.WriteLine(number + "-> нет");
---
+++ Task21/Program21.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве
+
+Console.WriteLine("Введите координаты первой точки, X: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты первой точки, Y: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты первой точки, Z: ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки, X: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки, Y: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки, Z: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+double temp = 0;
+CalcDistance(x1, y1, z1, x2, y2, z2);
+Console.Write("A(" + x1 + "," + y1 + "," + z1 + ")" + "; B(" + x2 + "," + y2 + "," + z2 + ") -> " + (result - result % 0.01));
+
+
+double CalcDistance(double arg1, double arg2, double arg3, double arg4, double arg5, double arg6)
+{
+    //temp = (((arg4 - arg1) * (arg4 - arg1)) + ((arg5 - arg2) * (arg5 - arg2)) + ((arg6 - arg3) * (arg6 - arg3)));
+    temp =(Math.Pow((arg4 - arg1),2) + Math.Pow((arg5 - arg2),2) + Math.Pow((arg6 - arg3),2));
+    //выше записан вариант без метода Math.Pow, но его запись длинее
+    //не знаю, какой способ оптимальнее (с точки зрения быстродействия) - с функцией или без
+    if (temp < 0)
+        temp = temp * -1; //проверка на отрицательное число и преобразование в положительное
+    result = Math.Sqrt(temp);
+    //чтобы из него можно было извлечь квадратный корень
+    return result;
+}
