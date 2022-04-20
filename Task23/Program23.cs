065b6b172bb3d7c6e95047d967f86ded92641c3d
aqxys <aqxyss@gmail.com>
2022-04-20 13:40:22 +0300

    Add homework 2004

---
+++ Task23/Program23.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int count = 1;
+
+Console.Write(number + " -> ");
+Coubline(number);
+
+int Coubline(int arg)
+{
+    while (count <= number)
+    {
+        result = count * count * count;
+        Console.Write(result + " ");
+        count++;
+    }
+    return result;
+}
--- lesson_2/Program.cs
+++ lesson_2/Program.cs
@@ -1,24 +1,19 @@
-﻿// Вторая лекция
-int Max(int arg1, int arg2, int arg3)
+﻿// Третий семинар
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+int y = 0;
+int count = 1;
+Console.Write("Число " + x + "\t" + "Таблица квадратов ");
+
+Quadrat(x);
+
+int Quadrat(int x)
 {
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
+    while (count <= x)
+    {
+        y = count * count;
+        Console.Write(y + " ");
+        count++;
+    }
+    return y;
 }
-int a1 = 13;
-int b1 = 16;
-int c1 = 19;
-int a2 = 11;
-int b2 = 1098;
-int c2 = 17;
-int a3 = 14;
-int b3 = 15;
-int c3 = 18;
-
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
-
-Console.WriteLine(max);
